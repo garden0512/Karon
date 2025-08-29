b4ff95f68486bf3f6562c6b8064eafc218a2763b
Garden1205 <12gundam05@gmail.com>
2025-08-30 04:22:59 +0900

    플레이어 이동 및 팔로잉카메라 도입

---
+++ KaronProject/Assets/Scripts/Prototype1/GameManager.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Karon.Prototype1
+{
+    public class GameManager : MonoBehaviour
+    {
+        public string TargetScene;
+
+        private void OnTriggerEnter2D(Collider2D collider)
+        {
+            SceneManager.LoadScene(TargetScene);
+        }
+
+    }
+}
