82395b263cb7ce6811546e8f52de1cff87d8c461
Garden0512 <12gundam05@gmail.com>
2026-05-18 19:05:07 +0900

    Feat: 경사로 미끄러짐 문제 해결

---
+++ UnityProject/My project/Assets/Scripts/Player/PlayerMove.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.InputSystem;
+
+namespace Karon.Player
+{
+    public class PlayerMove: MonoBehaviour
+    {
+        [SerializeField] private float moveSpeed = 5f;
+        [SerializeField] private float jumpForce = 10f;
+        [SerializeField] private Vector2 currentMovementInput;
+        private Rigidbody2D rb;
+
+        private void Awake()
+        {
+            rb = GetComponent<Rigidbody2D>();
+        }
+
+        private void FixedUpdate()
+        {
+            Move();
+        }
+
+        private void Move()
+        {
+            if (Mathf.Approximately(currentMovementInput.x, 0f))
+            {
+                rb.constraints = RigidbodyConstraints2D.FreezePositionX | RigidbodyConstraints2D.FreezeRotation;
+            }
+            else
+            {
+                rb.constraints = RigidbodyConstraints2D.FreezeRotation;
+            }
+            float direction = currentMovementInput.x * moveSpeed;
+            rb.linearVelocity = new Vector2(direction, rb.linearVelocity.y);
+        }
+
+        public void OnMove(InputValue value)
+        {
+            currentMovementInput = value.Get<Vector2>();
+        }
+
+        public void OnJump(InputValue value)
+        {
+            if (value.isPressed)
+            {
+                rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpForce);
+            }
+        }
+    }
+}
