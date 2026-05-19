ef011c51fd6e8a1e41346c5bcb0c9d983fe22269
garden0512 <12gundam05@gmail.com>
2026-05-20 01:27:09 +0900

    Feat: Input System 방식 변경

--- UnityProject/My project/Assets/Scripts/Player/PlayerMove.cs
+++ UnityProject/My project/Assets/Scripts/Player/PlayerMove.cs
@@ -5,29 +5,39 @@ using UnityEngine.InputSystem.Interactions;
 namespace Karon.Player
 {
     [RequireComponent(typeof(Rigidbody2D), typeof(CapsuleCollider2D))]
-    public class PlayerMove: MonoBehaviour
+    public class PlayerMove : MonoBehaviour, PlayerInputActions.IPlayerActions
     {
-        [Header("Movement Related Element")]
-        [SerializeField] private float moveSpeed = 5f;
-        [SerializeField] private float jumpForce = 10f;
-        [SerializeField] private Vector2 currentMovementInput;
-        [SerializeField] private Vector2 currentJumpInput;
-        [SerializeField] private double holdingTime;
-        private Rigidbody2D _rb;
+        [Header("Movement Related Element")] [SerializeField]
+        private float _moveSpeed = 5f;
 
-        [Header("CheckList")]
-        [SerializeField] private bool isGrounded;
-        [SerializeField] private bool isJumping;
-        
+        [SerializeField] private float _jumpForce = 10f;
+        [SerializeField] private Vector2 _currentMovementInput;
+        [SerializeField] private Vector2 _currentJumpInput;
+        [SerializeField] private double _holdingTime;
+        private Rigidbody2D _rb;
+        private PlayerInputActions _playerInputActions;
+
+        [Header("Raycast Related")] [SerializeField]
+        private LayerMask _groundLayer;
+
+        [Header("CheckList")] [SerializeField] private bool _isGrounded;
+        [SerializeField] private bool _isJumping;
+
         private void Awake()
         {
             _rb = GetComponent<Rigidbody2D>();
-            
+            _playerInputActions = new PlayerInputActions();
+            _playerInputActions.Player.SetCallbacks(this);
         }
 
-        private void Start()
+        private void OnEnable()
         {
-            
+            _playerInputActions.Enable();
+        }
+
+        private void OnDisable()
+        {
+            _playerInputActions.Disable();
         }
 
         private void FixedUpdate()
@@ -37,19 +47,19 @@ namespace Karon.Player
 
         private void Move()
         {
-            _rb.linearVelocity = new Vector2(currentMovementInput.x * moveSpeed, _rb.linearVelocity.y);
+            _rb.linearVelocity = new Vector2(_currentMovementInput.x * _moveSpeed, _rb.linearVelocity.y);
         }
 
         public void OnMove(InputAction.CallbackContext ctx)
         {
-            currentMovementInput = ctx.ReadValue<Vector2>();
+            _currentMovementInput = ctx.ReadValue<Vector2>();
         }
 
         public void OnJump(InputAction.CallbackContext ctx)
         {
-            if (ctx.performed && isGrounded)
+            if (ctx.performed && _isGrounded)
             {
-                _rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+                _rb.AddForce(Vector2.up * _jumpForce, ForceMode2D.Impulse);
             }
         }
 
@@ -57,15 +67,15 @@ namespace Karon.Player
         {
             if (collision.gameObject.CompareTag("Ground"))
             {
-                isGrounded = true;
+                _isGrounded = true;
             }
         }
-        
+
         public void OnCollisionExit2D(Collision2D collision)
         {
             if (collision.gameObject.CompareTag("Ground"))
             {
-                isGrounded = false;
+                _isGrounded = false;
             }
         }
     }
